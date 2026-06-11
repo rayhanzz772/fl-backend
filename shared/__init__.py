b74c7c2d47b145cff92e6a4b353d459694968a0d
rayhanzz772 <rayhanzz772@gmail.com>
2026-06-11 19:57:42 +0700

    refactor: remove COLUMN_CONFIG from shared module exports

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,10 +1,9 @@
-from .data_processor import StuntingDataProcessor, COLUMN_CONFIG
+from .data_processor import StuntingDataProcessor
 from .config import FederatedConfig
 from .utils import setup_logging, validate_data
 
 __all__ = [
     'StuntingDataProcessor',
-    'COLUMN_CONFIG', 
     'FederatedConfig',
     'setup_logging',
     'validate_data'
